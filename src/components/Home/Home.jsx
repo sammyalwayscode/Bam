31651e8e8787f585114171d94e02aea3eade3840
sammyalwayscode <olorundasamuel2@gmail.com>
2023-10-31 17:16:57 +0100

    small dust remain

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,14 +2,14 @@ import HeroPage from "./HeroPage";
 import Interview from "./Interview";
 import MenClub from "./MenClub";
 import Community from "./News/Community";
-import Latest from "./News/Latest";
+// import Latest from "./News/Latest";
 import Womenlub from "./Womenlub";
 
 const Home = () => {
   return (
     <div>
       <HeroPage />
-      <Latest />
+      {/* <Latest /> */}
       <Community />
       <Interview />
       <MenClub />
